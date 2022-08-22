911d96d13fa2592d2a0073c53274b37046de80b7
Jeffrey J. Desir <devops@informandinspire.llc>
2022-08-22 00:41:32 -0400

    [][][]

--- app/views/layouts/PageLayout/PageLayout.tsx
+++ app/views/layouts/PageLayout/PageLayout.tsx
@@ -9,7 +9,7 @@ const PageLayout = ({ children }) => {
     return (
         <RootLayout>
             <DrawerMenu />
-            <Header cta={{ name: '', href: ''}}navLinks={[{ name: '' }, { name: '' }]}
+            <Header cta={{ name: 'Support the MVMT', href: 'https://ko-fi.com/wtfmvmt'}}navLinks={[{ name: '' }, { name: '' }]}
                 favicon={{ src: 'https://static.shuffle.dev/uploads/files/c5/c588f2e6e97dec56040c09794a6a944110e98d9d/Untitled-design.png' }} />
             {children}
             <Footer copyright={" © 2022 WeTheFuture. All rights reserved."} />
