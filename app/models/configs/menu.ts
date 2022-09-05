8ea9e916b5b27dd033b42c0100f1b074a371b608
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-05 17:31:38 -0400

    xa

--- app/models/configs/menu.ts
+++ app/models/configs/menu.ts
@@ -1,8 +1,14 @@
 const menu = {
 
-favicon: {
-    url: '/assets/images/logo-transparent.png'
-}
+    favicon: {
+        url: '/assets/images/logo-transparent.png'
+    },
+    links: [
+        {
+            name: 'Home',
+            url: '/'
+        }
+    ]
 }
 
 export default menu
--- app/views/layouts/PageLayout/PageLayout.tsx
+++ app/views/layouts/PageLayout/PageLayout.tsx
@@ -4,11 +4,16 @@ import Header from "@components/Header"
 import DrawerMenu from "@views/includes/DrawerMenu";
 import { Fade } from "react-awesome-reveal";
 import menu from "@configs/menu"
+import Zoom from 'react-reveal/Zoom';
 
 const PageLayout = ({ children, metaData, header, footer }) => {
 
     return (
         <RootLayout metaData={metaData}>
+            <Zoom>
+
+                
+            </Zoom>
             <DrawerMenu {...menu} />
             <Header {...header} />
             <Fade>
