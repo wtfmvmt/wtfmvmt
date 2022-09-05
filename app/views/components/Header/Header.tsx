9fd771a1886caecf8f4597367e94d64679add52c
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-04 21:42:33 -0400

    ]

--- app/views/components/Header/Header.tsx
+++ app/views/components/Header/Header.tsx
@@ -3,6 +3,7 @@ import Headroom from "react-headroom";
 import Banner from "@components/Banner"
 import SearchIcon from "@components/SearchIcon";
 import BurgerIcon from "@components/BurgerIcon";
+import useMenuDrawerState from "@hooks/useMenuDrawerState"
 
 export type NavLink = {
     name: string,
@@ -23,6 +24,7 @@ export type Favicon = {
 export type BannerProps = {
     messages: string[]
 }
+
 export type HeaderProps = {
     favicon: Favicon,
     navLinks: NavLink[],
@@ -33,10 +35,21 @@ export type HeaderProps = {
 
 const Header = ({ favicon, navLinks, cta, banner }: HeaderProps) => {
 
+    const MobileNavBurger = () => (
+        <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
+            <a className="items-center pl-10 pr-2" href="#">
+                <BurgerIcon />
+            </a>
+        </a>
+    )
+
+    const { toggleMenu } = useMenuDrawerState()
+
     return (
         <section className="relative pb-20 overflow-hidden">
             <Headroom>
                 <Banner {...banner} />
+
                 <nav style={{ backdropFilter: 'blur(12px)' }} className="flex justify-between border-b">
                     <BurgerIcon />
                     <div className="px-12 py-6 flex w-full items-center">
@@ -82,22 +95,7 @@ const Header = ({ favicon, navLinks, cta, banner }: HeaderProps) => {
                     </a>
                     <a className="xl:hidden flex mr-6 items-center text-white" href="#">
                         <div className="xl:flex items-center">
-
-                            <a className="navbar-burger self-center mr-12 xl:hidden" href="#">
-                                <a className="items-center pl-10 pr-2" href="#">
-                                    <svg
-                                        width={18}
-                                        height={18}
-                                        viewBox="0 0 18 18"
-                                        fill="none"
-                                        xmlns="http://www.w3.org/2000/svg"
-                                    >
-                                        <path d="M18 15.4688H0V17.7207H18V15.4688Z" fill="white" />
-                                        <path d="M11.0226 7.87402H0V10.126H11.0226V7.87402Z" fill="white" />
-                                        <path d="M18 0.279297H0V2.53127H18V0.279297Z" fill="white" />
-                                    </svg>
-                                </a>
-                            </a>
+                          <MobileNavBurger/>
                         </div>
                     </a>
 
