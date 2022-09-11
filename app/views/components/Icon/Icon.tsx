84fb91434fb7f4dececf2c9e863ed1bc5977b41c
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-11 17:36:20 -0400

    rere

--- app/views/components/Icon/Icon.tsx
+++ app/views/components/Icon/Icon.tsx
@@ -1,17 +1,39 @@
 import dynamic from 'next/dynamic';
 
 const ICONS = {
-    'VISIBILITY': dynamic(() => import('@mui/icons-material/Visibility')),
-    'STORE': dynamic(() => import('@mui/icons-material/Store')),
-    'COLOR_LENS': dynamic(() => import('@mui/icons-material/ColorLens')),
-    'LIBRARY_BOOKS': dynamic(() => import('@mui/icons-material/LibraryBooks')),
-    'EVENT': dynamic(() => import('@mui/icons-material/Event')),
-    'HANDSHAKE': dynamic(() => import('@mui/icons-material/Handshake')),
-    'DIVERSITY': dynamic(() => import('@mui/icons-material/Diversity1')),
-    'MEDIA': dynamic(() => import('@mui/icons-material/PermMedia')),
-    'NEW': dynamic(() => import('@mui/icons-material/FiberNew')),
-    'DOMAIN': dynamic(() => import('@mui/icons-material/Domain')),
-    'VENDOR': dynamic(() => import('@mui/icons-material/StoreFront')),
+    'VISIBILITY': dynamic(() => import('@mui/icons-material/Visibility'), {
+        ssr: false
+    }),
+    'STORE': dynamic(() => import('@mui/icons-material/Store'),  {
+        ssr: false
+    }),
+    'COLOR_LENS': dynamic(() => import('@mui/icons-material/ColorLens'), {
+        ssr: false
+    }),
+    'LIBRARY_BOOKS': dynamic(() => import('@mui/icons-material/LibraryBooks'), {
+        ssr: false
+    }),
+    'EVENT': dynamic(() => import('@mui/icons-material/Event'), {
+        ssr: false
+    }),
+    'HANDSHAKE': dynamic(() => import('@mui/icons-material/Handshake'), {
+        ssr: false
+    }),
+    'DIVERSITY': dynamic(() => import('@mui/icons-material/Diversity1'), {
+        ssr: false
+    }),
+    'MEDIA': dynamic(() => import('@mui/icons-material/PermMedia'), {
+        ssr: false
+    }),
+    'NEW': dynamic(() => import('@mui/icons-material/FiberNew'), {
+        ssr: false
+    }),
+    'DOMAIN': dynamic(() => import('@mui/icons-material/Domain'), {
+        ssr: false
+    }),
+    'VENDOR': dynamic(() => import('@mui/icons-material/StoreFront'), {
+        ssr: false
+    }),
 }
 
 
