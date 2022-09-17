d4118a2bab73bbc485ac12ca339d0ee72fcc2153
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-16 20:22:08 -0400

    []

--- app/pages/api/facade.ts
+++ app/pages/api/facade.ts
@@ -1,5 +1,5 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
-import FacadeService from "@services/facade"
+import FacadeService from "@controllers/services/notion"
 
 export default async function handler(
     req: NextApiRequest,
