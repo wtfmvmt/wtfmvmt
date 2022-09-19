da0fae0b10ec83f79fdaed39672a64c2a34f5b32
Jeffrey Desir <8616589+desirtech@users.noreply.github.com>
2022-09-18 22:29:00 -0400

    [.]

--- app/views/includes/FloatingNavigator/FloatingNavigator.tsx
+++
@@ -1,3 +0,0 @@
-import SpeedDial, { SpeedDialProps } from '@mui/material/SpeedDial';
-import SpeedDialIcon from '@mui/material/SpeedDialIcon';
-import SpeedDialAction from '@mui/material/SpeedDialAction';
