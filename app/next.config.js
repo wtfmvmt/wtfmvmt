da6610f32b181f4b8cad01313eb6044444258267
Jeffrey J. Desir <devops@informandinspire.llc>
2022-11-05 21:39:19 -0500

    [ try pushing from VSCODE ]

--- app/next.config.js
+++ app/next.config.js
@@ -5,13 +5,12 @@ const withPWA = require("next-pwa");
 const nextConfig = {
   reactStrictMode: false,
   siteUrl: process.env.SITE_URL || 'https://wtfmvmt.com',
-  generateRobotsTxt: true, // (optional)
+  generateRobotsTxt: true, 
   swcMinify: true,
   images: {
     domains: ['scontent-akl1-1.cdninstagram.com', 's3.us-west-2.amazonaws.com'],
   },
   ...withPWA({
-
     pwa: {
       dest: "public",
       register: true,
