5d1c31b8615add5be18a27190876ef10426fef8b
Jeffrey J. Desir <devops@informandinspire.llc>
2022-11-22 15:12:35 -0500

    { It's not killing me now! }

--- app/views/layouts/PageLayout/PageLayout.tsx
+++ app/views/layouts/PageLayout/PageLayout.tsx
@@ -3,37 +3,32 @@ import Loader from "@components/Loader";
 import RootLayout from "@layouts/RootLayout";
 import type { LayoutProps } from "@typings/Layout";
 import type { NextPage } from "next";
-import React, { lazy, Suspense, useEffect } from "react";
-
-const Modal = lazy(() => import("@includes/Modal"));
-const Notifications = lazy(() => import("@includes/Notifications"));
-const DrawerMenu = lazy(() => import("@includes/DrawerMenu"));
-const MagicNavigator = lazy(() => import("@includes/MagicNavigator"));
-const Footer = lazy(() => import("@components/Footer"));
+import React, { lazy, Suspense } from "react";
+import Footer from "@components/Footer"
 
 const PageLayout: NextPage<LayoutProps> = ({ metaData, menu, header, footer, children }: LayoutProps) => {
 
-    useEffect(() => {
-        window.scrollTo({
-            top: 0,
-            left: 0,
-            behavior: 'smooth'
-        });
-    }, [])
+
+    const Modal = lazy(() => import("@includes/Modal"));
+    const Notifications = lazy(() => import("@includes/Notifications"));
+    const DrawerMenu = lazy(() => import("@includes/DrawerMenu"));
+    const MagicNavigator = lazy(() => import("@includes/MagicNavigator"));
 
 
     return (
-        <Suspense fallback={<Loader />}>
+        <>
             <RootLayout metaData={metaData}>
                 <Header {...header} />
                 {children}
                 <Footer {...footer} />
             </RootLayout>
-            <MagicNavigator />
-            <Modal />
-            <Notifications />
+            <Suspense fallback={<Loader />}>
+                <MagicNavigator />
+                <Modal />
+                <Notifications />
+            </Suspense>
             <DrawerMenu {...menu} />
-        </Suspense>
+        </>
     )
 }
 
