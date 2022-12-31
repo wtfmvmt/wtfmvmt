b734f817fa0a55302aead591c13af9c2bc510da9
Jeffrey J. Desir <devops@informandinspire.llc>
2022-12-31 12:34:41 -0500

    { SPRINT!!!! }

---
+++ app/stories/data/colors.ts
@@ -0,0 +1,15 @@
+export const colors = {
+    cards: [
+        {
+            title: 'Primary Color',
+            subHeading: '#80800',
+            actions: [
+                {
+                    name: "Copy HEX Code",
+                    icon: '🖨️'
+                }
+            ]
+
+        }
+    ]
+}
---
+++ app/stories/data/home.ts
@@ -0,0 +1,123 @@
+export const home = {
+    cards: [
+        {
+            title: 'Facade',
+            heading: " This is where all org data is centralized. Space allocation is virtually infinite. Use wisely",
+            subHeading: 'Visual Database',
+            actions: [
+                {
+                    name: 'Open in New Tab'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        }, {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        },
+        {
+            title: 'Meta',
+            subHeading: "Facade Database",
+            heading: "This is where all org meta-data is centralized. Copyright, logos, language, etc...",
+            actions: [
+                {
+                    name: 'Open in New Tab',
+                    icon: '📂'
+                }
+            ]
+        }
+    ]
+}
