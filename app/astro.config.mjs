ecbfaf3f676ce3b624d05e6e93d2d32053823607
Jeffrey J. Desir <devops@informandinspire.llc>
2023-01-30 14:46:46 -0500

    [][][][]

--- app/astro.config.mjs
+++ app/astro.config.mjs
@@ -1,22 +1,14 @@
 import { defineConfig } from "astro/config";
 
-// https://astro.build/config
-
-// https://astro.build/config
 import vercel from "@astrojs/vercel/serverless";
 
-// https://astro.build/config
 import mdx from "@astrojs/mdx";
 import prefetch from "@astrojs/prefetch";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 
-// https://astro.build/config
-
-// https://astro.build/config
 import react from "@astrojs/react";
 
-// https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), sitemap({
     canonicalURL: 'https://astrojs.com'
