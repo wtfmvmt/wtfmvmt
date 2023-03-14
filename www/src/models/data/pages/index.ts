621e168b7b8c1eb373b3c988d2476f307ff6bbcb
Jeffrey J. Desir <devops@informandinspire.llc>
2023-03-13 21:13:52 -0400

    @

--- www/src/models/data/pages/index.ts
+++ www/src/models/data/pages/index.ts
@@ -1,66 +1,21 @@
+import { defineViewStore } from "blakprint/dist/models"
 
-import { pages as pagesUtils } from '@utils/index'
 
-export const defineViewStore = () => {
-    return (
-        [
-            0
-        ]
-    )
-}
-export default async function pagesQuery({ id }) {
-
-    const { generatePage } = pagesUtils()
-
-    const sitePagesStore = {
-        layout: {
-            header: {
-                favicon: {
-                    image: {
-                        src: ""
-                    }
-                },
-                links: [
-                    {
-                        name: "Partners",
-                        url: "#partners"
-                    },
-                    {
-                        name: "Events",
-                        url: "#events"
-                    }
-                ]
-            }
-        },
-        pages: {
-            home: {
-                metaData: {
-                    pageTitle: "Home"
-                },
-                data: {
-                    hero: {
-                        heading: "",
-                        title: "We the Future MVMT",
-                        description: "",
-                        cover: {
-                            src: "https://res.cloudinary.com/facade-os/image/upload/v1675361093/Fa%C3%A7ade/WTFMVMT/media/Group_Photo_of_WTFMVMT_Members_with_Goofy_Faces_p6flqk.jpg"
-                        }
-                    },
-
-                },
-
-            },
-            about: {},
-            software: {},
-            writings: {},
-            sectors: {},
-            resources: {},
-        }
+const SitePages = defineViewStore({
+    pages: {
+        home: {},
+        memberships: {},
+        events: {},
+        partners: {},
+    },
+    layout: {
+        metaData: {},
+        header: {},
+        footer: {}
     }
+})
 
-    return generatePage({ store: sitePagesStore, id })
-}
-
+export default SitePages
 
 
 
